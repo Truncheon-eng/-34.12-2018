dba477ded71c9a1ed9c7faf7714127ec1037a164
Truncheon-eng <ghost2019krolick@gmail.com>
2024-06-12 00:55:18 +0300

    Added documantation

---
+++ encryption_and_decryption/documentation/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mainframe_0',['MainFrame',['../class_main_frame.html',1,'MainFrame'],['../class_main_frame.html#a00c85521dad6b24000967206bcc53688',1,'MainFrame::MainFrame()']]]
+];
---
+++ encryption_and_decryption/documentation/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "amo",
+  1: "am",
+  2: "mo"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Классы",
+  2: "Функции"
+};
+
