a449a61a9ac56fe941bb904cde81f94e26b352f0
Truncheon-eng <ghost2019krolick@gmail.com>
2024-06-14 22:52:05 +0100

    Final formatting

--- encryption_and_decryption/classes/MockMsgBox.h
+++ encryption_and_decryption/classes/MockMsgBox.h
@@ -14,18 +14,6 @@
 /**
  * @brief Класс, необходимый для имитирования функции wxMessageBox.
  */
-class MockMsgBox: public IMsgBox{
-public:
-	/**
-	 * @brief mock метод, необходимый для проведения mock тестов. 
-	 * @param message текст, выводящийся в окне-сообщении.
-	 * @param caption текст, выводящийся в верхней панели окна.
-	 * @param style переменная, задающая внешний вид, значение кнопок окна-сообщения.
-	 * @param parent указатель на родителя окна-сообщения.
-	 * @return идентификаторы кнопок(например wxYes, wxNo), так как wxMessageBox 
-	 * также их и возвращает.
-	 */
-	MOCK_METHOD(int, Show, (const wxString &message, const wxString &caption, int style, wxWindow *parent), (override));
 class MockMsgBox : public IMsgBox {
   public:
     /**
@@ -35,13 +23,29 @@ class MockMsgBox : public IMsgBox {
      * @param style переменная, задающая внешний вид, значение кнопок
      * окна-сообщения.
      * @param parent указатель на родителя окна-сообщения.
-     * @return тип данных: int, так как wxMessageBox также возвращает значения
-     * типа int.
+     * @return идентификаторы кнопок(например wxYes, wxNo), так как wxMessageBox
+     * также их и возвращает.
      */
     MOCK_METHOD(int, Show,
                 (const wxString &message, const wxString &caption, int style,
                  wxWindow *parent),
                 (override));
-};
+    class MockMsgBox : public IMsgBox {
+      public:
+        /**
+         * @brief mock метод, необходимый для проведения mock тестов.
+         * @param message текст, выводящийся в окне-сообщении.
+         * @param caption текст, выводящийся в верхней панели окна.
+         * @param style переменная, задающая внешний вид, значение кнопок
+         * окна-сообщения.
+         * @param parent указатель на родителя окна-сообщения.
+         * @return тип данных: int, так как wxMessageBox также возвращает
+         * значения типа int.
+         */
+        MOCK_METHOD(int, Show,
+                    (const wxString &message, const wxString &caption,
+                     int style, wxWindow *parent),
+                    (override));
+    };
 
 #endif
--- encryption_and_decryption/classes/RealMsgBox.h
+++ encryption_and_decryption/classes/RealMsgBox.h
@@ -12,18 +12,6 @@
 /**
  * @brief Класс, необходимый для вывода окна-сообщения.
  */
-class RealMsgBox: public IMsgBox {
-public:
-	/**
-	 * @brief метод, вызываемый аттрибутом m_msg_box класса MainFrame для создания окна-сообщения. 
-	 * @param message текст, выводящийся в окне-сообщении.
-	 * @param caption текст, выводящийся в верхней панели окна.
-	 * @param style переменная, задающая внешний вид, значение кнопок окна-сообщения.
-	 * @param parent указатель на родителя окна-сообщения.
-	 * @return идентификаторы кнопок(например wxYes, wxNo), так как wxMessageBox 
-	 * также их и возвращает.
-	 */
-	int Show(const wxString &message, const wxString &caption, int style, wxWindow *parent) override;
 class RealMsgBox : public IMsgBox {
   public:
     /**
@@ -34,11 +22,26 @@ class RealMsgBox : public IMsgBox {
      * @param style переменная, задающая внешний вид, значение кнопок
      * окна-сообщения.
      * @param parent указатель на родителя окна-сообщения.
-     * @return тип данных: int, так как wxMessageBox также возвращает значения
-     * типа int.
+     * @return идентификаторы кнопок(например wxYes, wxNo), так как wxMessageBox
+     * также их и возвращает.
      */
     int Show(const wxString &message, const wxString &caption, int style,
              wxWindow *parent) override;
-};
+    class RealMsgBox : public IMsgBox {
+      public:
+        /**
+         * @brief метод, вызываемый аттрибутом m_msg_box класса MainFrame для
+         * создания окна-сообщения.
+         * @param message текст, выводящийся в окне-сообщении.
+         * @param caption текст, выводящийся в верхней панели окна.
+         * @param style переменная, задающая внешний вид, значение кнопок
+         * окна-сообщения.
+         * @param parent указатель на родителя окна-сообщения.
+         * @return тип данных: int, так как wxMessageBox также возвращает
+         * значения типа int.
+         */
+        int Show(const wxString &message, const wxString &caption, int style,
+                 wxWindow *parent) override;
+    };
 
 #endif
